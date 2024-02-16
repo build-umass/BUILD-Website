a878074e7c53bc1fa2ec36d6e836ec0e4b58b781
yonava <yona@vossandreae.com>
2024-02-15 23:51:14 -0500

    fixed company logos!

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+export default function CompanyLogos() {
+
+  const logoPath = "img/logo/";
+
+  const logos = [
+    {
+      name: 'Microsoft',
+      src: 'msft.svg',
+    },
+    {
+      name: 'Facebook',
+      src: 'fb.svg',
+    },
+    {
+      name: 'Google',
+      src: 'goog.svg',
+    },
+    {
+      name: 'Amazon',
+      src: 'Amazon_logo.svg',
+    },
+    {
+      name: 'Uber',
+      src: 'uber.svg',
+    },
+    {
+      name: 'Cisco',
+      src: 'cisco.svg',
+    },
+    {
+      name: 'Visa',
+      src: 'visa.png',
+    },
+    {
+      name: 'Mastercard',
+      src: 'mastercard.svg',
+    },
+    {
+      name: 'Tanium',
+      src: 'tanium.png',
+    },
+    {
+      name: 'Fidelity',
+      src: 'fidelity.svg',
+    },
+    {
+      name: 'Pratt and Whitney',
+      src: 'pratt-whitney.svg',
+    },
+    {
+      name: 'Dell',
+      src: 'dell.png',
+    },
+    {
+      name: 'MassMutual',
+      src: 'massmutual.svg',
+    },
+    {
+      name: 'Liberty Mutual',
+      src: 'liberty-mutual.png',
+    },
+    {
+      name: 'Blue Cross',
+      src: 'blue-cross-blue-shield.png',
+    },
+  ];
+
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
+      gap: "100px"
+    }}>
+      {logos.map((logo, index) => {
+        return (
+          <img key={index} src={logoPath + logo.src} alt={logo.name} style={{
+            width: "300px",
+            height: "150px",
+            objectFit: "contain",
+          }} />
+        )
+      })}
+    </div>
+  );
+}
